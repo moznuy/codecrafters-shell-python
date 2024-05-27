9cfb73c5ea6c98b8d93a61cd04d0a3c0034baa66
Serhii Charykov <serhii.charykov@gmail.com>
2024-05-27 14:04:57 +0300

    pass 4th stage

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 import sys
 
 
-COMMAND_MAP = {}
+def cmd_exit(*params):
+    exit_code = 0
+    if params:
+        exit_code = int(params[0])
+    sys.exit(exit_code)
+
+
+COMMAND_MAP = {"exit": cmd_exit}
 
 
 def main():
@@ -19,6 +26,7 @@ def main():
             sys.stderr.write(f"{command}: command not found\n")
             sys.stderr.flush()
             continue
+        fn(*params)
 
 
 if __name__ == "__main__":
