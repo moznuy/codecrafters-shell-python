94e8b797d07b497ae32719284018d9d6bf22eef8
Serhii Charykov <serhii.charykov@gmail.com>
2024-05-31 17:52:18 +0300

    pass 10th stage

--- app/main.py
+++ app/main.py
@@ -52,9 +52,9 @@ def cmd_cd(*params):
     # TODO: too many arguments
     path = os.path.expanduser(to)
     if not os.path.exists(path):
-        # TODO: stderr
-        sys.stdout.write(f"cd: {path}: No such file or directory\n")
-        sys.stdout.flush()
+        # TODO: cd: {path}:
+        sys.stderr.write(f"{path}: No such file or directory\n")
+        sys.stderr.flush()
         return
     os.chdir(path)
 
